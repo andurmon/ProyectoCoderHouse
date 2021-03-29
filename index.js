193368dffcdf0b902859343faa5cae859f9b5d88
andurmon <andurmon@gmail.com>
2021-03-29 14:26:33 -0500

    Se terminan pruebas unitarias de la API

--- index.js
+++ index.js
@@ -12,22 +12,10 @@ const {engineEJS: engine} = require("./routes/engine");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.get('/styles/chat.css', (req, res)=>{
-    res.sendFile(__dirname + '/public/styles/chat.css');
-})
 
 require("./sockets/sockets")(io)
 
 app.set("views", "./views");
-// app.engine("hbs",
-//     handlebars({
-//         extname:".hbs",
-//         defaultLayout:"vistaProductos.hbs",
-//         layoutsDir:__dirname+"/views/layouts/",
-//         partialsDir:__dirname +"/views/partials/"
-//     })
-// )
-// app.set("view engine", "hbs");
 
 app.set("view engine", "ejs")
 app.get('/productos/vista', engine);
